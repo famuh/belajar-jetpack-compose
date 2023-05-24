e36a08de358c447a1f448ee26bd9c2b499ce7277
famuh <famuh79@gmail.com>
2023-05-24 23:51:35 +0700

    - Membuat view sederhana

--- app/src/main/java/com/example/hellojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/hellojetpackcompose/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.hellojetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,15 +35,38 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.jetpack_compose),
+            contentDescription = "Logo Jetpack Compose",
+            modifier = Modifier.size(80.dp)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = "Welcome to Dicoding !")
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloJetpackComposeTheme {
-        Greeting("Android")
+        Greeting("Jetpack Compose")
     }
 }
