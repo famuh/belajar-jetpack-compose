af1ce356143cc58d6bb86e46e694214cf7822180
famuh <famuh79@gmail.com>
2023-05-28 15:18:42 +0700

    - membuat komponen menu item

--- JetCoffee/app/src/main/java/com/example/jetcoffee/MainActivity.kt
+++ JetCoffee/app/src/main/java/com/example/jetcoffee/MainActivity.kt
@@ -14,11 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetcoffee.components.CategoryItem
 import com.example.jetcoffee.components.SearchBar
+import com.example.jetcoffee.components.SectionText
 import com.example.jetcoffee.model.dummyCategory
 import com.example.jetcoffee.ui.theme.JetCoffeeTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
@@ -39,6 +41,8 @@ class MainActivity : ComponentActivity() {
 fun JetCoffeeApp() {
     Column {
         Banner()
+        SectionText(stringResource(id = R.string.section_category))
+        CategoryRow()
     }
 }
 
